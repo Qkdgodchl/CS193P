55a58a6e8ad97f4fa8a78d99ce75c031d5566daf
이영민 <fkvkxk1@naver.com>
2026-02-04 05:18:48 +0900

    Add Assignment 0 project

---
+++ cs193p/cs193pTests/cs193pTests.swift
@@ -0,0 +1,16 @@
+//
+//  cs193pTests.swift
+//  cs193pTests
+//
+//  Created by 이영민 on 2/4/26.
+//
+
+import Testing
+
+struct cs193pTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
