2335c55cb97e3da2d8ea9c63c0372b7c5c2e121a
이영민 <fkvkxk1@naver.com>
2026-02-04 05:18:48 +0900

    Add Assignment 0 project

---
+++ cs193p/cs193p/cs193pApp.swift
@@ -0,0 +1,17 @@
+//
+//  cs193pApp.swift
+//  cs193p
+//
+//  Created by 이영민 on 2/4/26.
+//
+
+import SwiftUI
+
+@main
+struct cs193pApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ cs193p/cs193pUITests/cs193pUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  cs193pUITestsLaunchTests.swift
+//  cs193pUITests
+//
+//  Created by 이영민 on 2/4/26.
+//
+
+import XCTest
+
+final class cs193pUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
